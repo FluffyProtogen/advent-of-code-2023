87cf6b945a36f014d9d26819548d5ead4b52e602
Tecky <programmingfrog@gmail.com>
2023-12-01 00:20:35 -0500

    finished

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let total = include_str!("../input.txt")
+        .lines()
+        .map(|line| {
+            let mut numbers = line.chars().map(|char| char.to_digit(10)).flatten();
+            let first = numbers.nth(0).unwrap();
+            first * 10 + numbers.last().unwrap_or(first)
+        })
+        .sum::<u32>();
+    println!("{total}");
 }
