7c9f8fa0dc564da1a4fbaa0fd6278a9d39d27564
Tecky <programmingfrog@gmail.com>
2023-12-04 00:28:28 -0500

    finished

---
+++ day04a/src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashSet;
+
+fn main() {
+    let total = include_str!("../input.txt")
+        .lines()
+        .map(|line| {
+            let line = line.split_once(": ").unwrap().1;
+            let (winning_numbers, held_numbers) = line.split_once(" | ").unwrap();
+
+            let winning_numbers = winning_numbers
+                .split(" ")
+                .flat_map(|line| line.parse::<u8>())
+                .collect::<HashSet<_>>();
+
+            let wins = held_numbers
+                .split(" ")
+                .flat_map(|line| line.parse())
+                .filter(|number| winning_numbers.contains(number))
+                .count();
+
+            if wins == 0 {
+                0
+            } else {
+                2u32.pow(wins as u32 - 1)
+            }
+        })
+        .sum::<u32>();
+
+    println!("{total}");
+}
