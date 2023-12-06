e549d5535884f14b437f05556dc03c31ec2cddf5
Tecky <programmingfrog@gmail.com>
2023-12-06 09:50:38 -0500

    finished

---
+++ day06b/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let (total_time, distance) = include_str!("../input.txt").split_once('\n').unwrap();
+    let total_time = total_time.split_once(": ").unwrap().1;
+    let distance = distance.split_once(": ").unwrap().1;
+    let total_time = total_time.replace(" ", "").parse().unwrap();
+    let distance = distance.replace(" ", "").parse::<u64>().unwrap();
+    let possibilities = (0..total_time)
+        .filter(|time| (total_time - time) * time > distance)
+        .count();
+    println!("{possibilities}");
+}
